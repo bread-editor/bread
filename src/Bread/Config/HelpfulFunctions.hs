013e09fd180bf775bc1c85f9f08caf7d072fc447
zovt <zovtly@gmail.com>
2015-07-23 14:28:32 -0400

    Nuke repo

--- src/Bread/Config/HelpfulFunctions.hs
+++
@@ -1,9 +0,0 @@
--- | Convenience functions for use in configuration
-module Bread.Config.HelpfulFunctions ( oMap ) where
-
-import Data.HashMap as HM
-import Data.Hashable
-
--- | Convenience function for making an options map
-oMap :: (Ord k, Hashable k) => [(k, v)] -> HM.Map k v
-oMap = HM.fromList
--- src/Main.hs
+++
@@ -1,4 +0,0 @@
-import Bread.Server
-import qualified Bread.Core.Main
-
-main = Bread.Core.Main.main
