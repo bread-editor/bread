57434c93e496e574b55295878bf23f31bee0918b
zovt <zovtly@gmail.com>
2015-10-01 23:31:09 +0000

    Added API Calls. Added Reading of Arbitrary length requests.

--- communication/hooks.go
+++ communication/hooks.go
@@ -1,5 +1,7 @@
 package communication
 
+import ()
+
 type Hook struct {
 	// Hook name
 	Name string
--- server/client.go
+++ server/client.go
@@ -1,28 +1,12 @@
 package server
 
-// Used to represent the two types of clients that exist in Bread.
-//
-// PASSIVE clients WILL NOT change the state of the editor themselves. They
-// simply monitor the editor state with various hooks. They can, however, run
-// any other programs outside of themselves, including other Bread clients. Each
-// call that a PASSIVE client makes will be executed asynchronously by default.
-//
-// ACTIVE clients WILL change the state of the editor themselves. Each call that
-// an ACTIVE client makes will be executed synchronously by default.
-type ClientType int
-
-const (
-	ACTIVE ClientType = iota
-	PASSIVE
-)
-
 type Client struct {
 	// Did the Client send the proper initialization request?
 	Initialized bool
 
-	// Client type
-	Type ClientType
-
 	// Is the client connected?
 	Connected bool
+
+	// Hooks the client is listening for
+	Hooks string
 }
